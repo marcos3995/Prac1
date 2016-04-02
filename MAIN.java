83b377119d259f2dc16c11c5a8a1a939d11863d4
AnaVillalon <Anaesperanzavillalon@hotmail.com>
2016-04-02 13:42:47 +0200

    Es la parte B

--- MAIN.java
+++ MAIN.java
@@ -12,16 +12,13 @@ public class Jarra {
  }
 //  metodos
               1
-              2
-               public void llenaDesde(Jarra j){
-		while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
-			this.cantidad++;
-			j.cantidad--;
-		} 
-	
-	public String toString(){
-		return ("("+this.capacidad+","+this.cantidad+")");
-	}
+              public void llena(){
+  this.cantidad=this.capacidad;
+ }
+public void vacia(){
+  this.cantidad=0;
+ }
+              3
 
         public void metodoComunitario(){ 
 // Comentario incluido para pruebas hola
